3940adcc8838e55cc3acf2c48ee78c3aea67015b
Frixxie <freddahf@outlook.com>
2021-02-03 11:03:20 +0100

    added setup.py and made it a pip package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='uit_calendar_util',
+    version='0.1.0',
+    description='A calendar utility aimed for UiT students',
+    url='https://github.com/Frixxie/uit_calendar_util.git',
+    author='Fredrik Hagen Fasteraune',
+    author_email="freddahf@outlook.com",
+    license='',
+    packages=['uit_calendar_util'],
+    install_requires=['ics',
+                    'requests',
+                    ],
+    classifiers=[
+        'Developing'
+    ],
+)
