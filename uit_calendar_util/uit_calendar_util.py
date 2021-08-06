cd1078eb82b161b73ec2d9fb215509329ba78fc4
Fredrik <freddahf@outlook.com>
2021-08-06 10:53:51 +0200

    rewrote internal filtering

--- uit_calendar_util/uit_calendar_util.py
+++ uit_calendar_util/uit_calendar_util.py
@@ -45,6 +45,13 @@ class Calendar_util:
         Populates the events in the calendar_util grouped by lecture and not lecture
         And sorts the list by unixtime stamp
         """
+
+        """
+        So hear me out, it is better to declare the functions here instead
+        of elsewhere and why noy use lambda? man look at the functions
+        they are massive and it is better to use functions in map instead of
+        lambdas even though it is considered "unpythonic"
+        """
         def _create_event(event):
             return Event(event.name, event.begin.timestamp, event.description, True) if re.search(r'Forelesning', event.description, re.M | re.I) or re.search(r'Lecture', event.description, re.M | re.I) else Event(event.name, event.begin.timestamp, event.description, False)
 
@@ -82,85 +89,65 @@ class Calendar_util:
         """
         time_now = int(time.time())
 
-        """
-        So hear me out, it is better to declare the functions here instead
-        of elsewhere and why noy use lambda? man look at the functions
-        they are massive and it is better to use functions in map instead of
-        lambdas even though it is considered "unpythonic"
-        """
         def _filter_func(event):
             return event if event.timestamp - time_now > 0 and event.timestamp - time_now <= lim and event.lecture else None
 
-        upcoming_events = list(
-            filter(_filter_func, self.events))
-        return upcoming_events
+        return list(filter(_filter_func, self.events))
+
+    def get_next_upcoming_timestamp(self) -> int:
+        time_now = int(time.time())
+        for event in self.events:
+            if event.timestamp - time_now > 0:
+                return event.timestamp
+        return -1
 
     def get_next_upcoming_lecture(self):
         """
         Gets the next lecture independent of time
         Will always return a list
         """
-        time_now = int(time.time())
-        upcoming_events = list()
-        for i, event in enumerate(self.events):
-            if event.timestamp - time_now > 0 and event.lecture:
-                upcoming_events.append(event)
-                j = i
-                next_event = self.events[j + 1]
-                while event.timestamp == next_event.timestamp:
-                    next_event = self.events[j]
-                    if next_event.lecture:
-                        upcoming_events.append(next_event)
-                    j += 1
-                break
-        return upcoming_events
+        timestamp = self.get_next_upcoming_timestamp()
+
+        def _filter_func(event):
+            return event if event.timestamp == timestamp and event.lecture else None
+
+        return list(filter(_filter_func, self.events))
 
     def get_next_event(self, lim=60*15, filtr=[], is_lecture=True):
         """
         Gets the next event, can filter by lecure and filters
         """
         time_now = int(time.time())
-        upcoming_events = list()
-        for event in self.events:
+
+        def _filter_func(event):
             if event.timestamp - time_now > 0 and event.timestamp - time_now <= lim and event.lecture == is_lecture:
                 if len(filtr) > 0:
                     for name in filtr:
                         if self.check_filtr(name, event):
-                            upcoming_events.append(event)
+                            return event
                 else:
-                    upcoming_events.append(event)
-            if event.timestamp - time_now > lim:
-                break
-        return upcoming_events
+                    return event
+            return None
+
+        return list(filter(_filter_func, self.events))
 
     def get_next_upcoming_event(self, filtr=[], is_lecture=True):
         """
         Same as above independent of time
         """
-        time_now = int(time.time())
-        upcoming_events = list()
-        for i, event in enumerate(self.events):
-            if event.timestamp - time_now > 0 and event.lecture == is_lecture:
+        timestamp = self.get_next_upcoming_timestamp()
+
+        def _filter_func(event):
+            if event.timestamp == timestamp and event.lecture == is_lecture:
                 if len(filtr) > 0:
                     for name in filtr:
                         if self.check_filtr(name, event):
-                            upcoming_events.append(event)
+                            return event
                 else:
-                    upcoming_events.append(event)
-                j = i
-                next_event = self.events[j + 1]
-                while event.timestamp == next_event.timestamp:
-                    next_event = self.events[j]
-                    if next_event.lecture == is_lecture:
-                        if len(filtr) > 0:
-                            for name in filtr:
-                                if self.check_filtr(next_event, name):
-                                    upcoming_events.append(next_event)
-                    else:
-                        upcoming_events.append(next_event)
-                    j += 1
-                break
-        return upcoming_events
+                    return event
+            return None
+
+        return list(filter(_filter_func, self.events))
 
 
 if __name__ == '__main__':
@@ -174,4 +161,4 @@ if __name__ == '__main__':
     # The next lecures for the next 24 hours
     print(len(cu.events))
     print(cu.get_next_lecture(60*60*24*14))
-    print(cu.get_next_upcoming_event(is_lecture=True))
+    print(cu.get_next_upcoming_lecture())
