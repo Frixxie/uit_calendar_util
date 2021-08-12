13d8ce49782446b9aa6e95a61c017a36bbf98e23
Fredrik <freddahf@outlook.com>
2021-08-12 11:00:03 +0200

    updated example to more recent one

--- example.py
+++ example.py
@@ -4,9 +4,13 @@ if __name__ == '__main__':
     """
     Example code
     """
-    courses = ["INF-2900-1", "INF-2310-1", "INF-1400-1", "MAT-2300-1", "MAT-1002-1", "FIL-0700-1", "BED-2017-1"]
-    url = "https://timeplan.uit.no/calendar.ics?sem=21v"
+    courses = ["INF-3201-1", "INF-3200-1",
+               "FYS-2021-1", "INF-2700-1", "INF-1049-1"]
+    url = "https://timeplan.uit.no/calendar.ics?sem=21h"
     cu = Calendar_util(url, courses)
     # The next lecures for the next 24 hours
-    print(cu.get_next_lecture(60*60*24*3))
-    print(cu.get_next_event(60*60*24*3, is_lecture = True))
+    print(len(cu.events))
+    print(cu.get_next_lecture(60*60*24*14))
+    print(cu.get_next_event(60*60*24*14, is_lecture=True))
+    print(cu.get_next_upcoming_lecture())
+    print(cu.get_next_upcoming_event(is_lecture=True))
