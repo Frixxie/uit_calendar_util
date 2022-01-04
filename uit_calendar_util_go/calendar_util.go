f9814bb1cab9b8aa3ca295c0670554d51411f83a
fredrik <freddahf@outlook.com>
2022-01-04 19:03:40 +0100

    i am doing cool stuffies

--- uit_calendar_util_go/calendar_util.go
+++ uit_calendar_util_go/calendar_util.go
@@ -6,47 +6,87 @@ import (
 	"io"
 	"net/http"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	ics "github.com/erizocosmico/go-ics"
 	"github.com/gocarina/gocsv"
 )
 
+type TimeStamp struct {
+	time.Time
+}
+
+func (t *TimeStamp) MarshalCSV() (string, error) {
+	return t.String(), nil
+}
+
+func (t *TimeStamp) String() string {
+	return t.Time.String()
+}
+
+func (t *TimeStamp) UnmarshalCSV(csv string) (err error) {
+	date_str := strings.Split(strings.Replace(strings.Replace(csv, "-", " ", 3), ":", " ", 1), " ")
+	year, y_err := strconv.Atoi(date_str[0])
+	if y_err != nil {
+		return y_err
+	}
+	month, m_err := strconv.Atoi(date_str[1])
+	if m_err != nil {
+		return m_err
+	}
+	day, d_err := strconv.Atoi(date_str[2])
+	if d_err != nil {
+		return d_err
+	}
+	hour, h_err := strconv.Atoi(date_str[3])
+	if h_err != nil {
+		return h_err
+	}
+	minute, m_err := strconv.Atoi(date_str[4])
+	if m_err != nil {
+		return m_err
+	}
+	t.Time = time.Date(year, time.Month(month), day, hour, minute, 0, 0, time.Local)
+	return nil
+}
+
 type CsvEvent struct {
-	SemesterId         string `csv:"semesterid"`
-	CourseId           string `csv:"courseid"`
-	CourseVersion      string `csv:"courseversion"`
-	Actid              string `csv:"actid"`
-	ID                 string `csv:"id"`
-	WeekNr             string `csv:"weeknr"`
-	DtStart            string `csv:"dtstart"`
-	DtEnd              string `csv:"dtend"`
-	lopenr             string `csv:"lopenr"`
-	TeachingMethod     string `csv:"teaching-method"`
-	TeachingMethodName string `csv:"teaching-method-name"`
-	TeachingTitle      string `csv:"teaching-title"`
-	Summary            string `csv:"summary"`
-	StatusPlenary      string `csv:"status_plenary"`
-	Staffs             string `csv:"staffs"`
-	StudentGroups      string `csv:"studentgroups"`
-	Room               string `csv:"room"`
-	Terminnr           string `csv:"terminnr"`
-	Aid                string `csv:"aid"`
-	Compulsory         string `csv:"compulsory"`
-	Discipline         string `csv:"discipline"`
-	DisciplineObj      string `csv:"disciplineobj"`
-	Resources          string `csv:"resources"`
-	Alerts             string `csv:"alerts"`
-	Staffnames         string `csv:"staffnames"`
-	Editurl            string `csv:"editurl"`
-	Weekday            string `csv:"weekday"`
-	EventID            string `csv:"eventid"`
-	Multiday           string `csv:"multiday"`
-	AllWeeks           string `csv:"allweeks"`
-	Followers          string `csv:"followers"`
-	Curr               string `csv:"curr"`
-	Tags               string `csv:"tags"`
-	Party              string `csv:"party"`
+	SemesterId         string    `csv:"semesterid"`
+	CourseId           string    `csv:"courseid"`
+	CourseVersion      string    `csv:"courseversion"`
+	Actid              string    `csv:"actid"`
+	ID                 string    `csv:"id"`
+	WeekNr             string    `csv:"weeknr"`
+	DtStart            TimeStamp `csv:"dtstart"`
+	DtEnd              TimeStamp `csv:"dtend"`
+	lopenr             string    `csv:"lopenr"`
+	TeachingMethod     string    `csv:"teaching-method"`
+	TeachingMethodName string    `csv:"teaching-method-name"`
+	TeachingTitle      string    `csv:"teaching-title"`
+	Summary            string    `csv:"summary"`
+	StatusPlenary      string    `csv:"status_plenary"`
+	Staffs             string    `csv:"staffs"`
+	StudentGroups      string    `csv:"studentgroups"`
+	Room               string    `csv:"room"`
+	Terminnr           string    `csv:"terminnr"`
+	Aid                string    `csv:"aid"`
+	Compulsory         string    `csv:"compulsory"`
+	Discipline         string    `csv:"discipline"`
+	DisciplineObj      string    `csv:"disciplineobj"`
+	Resources          string    `csv:"resources"`
+	Alerts             string    `csv:"alerts"`
+	Staffnames         string    `csv:"staffnames"`
+	Editurl            string    `csv:"editurl"`
+	Weekday            string    `csv:"weekday"`
+	EventID            string    `csv:"eventid"`
+	Multiday           string    `csv:"multiday"`
+	AllWeeks           string    `csv:"allweeks"`
+	Followers          string    `csv:"followers"`
+	Curr               string    `csv:"curr"`
+	Tags               string    `csv:"tags"`
+	Party              string    `csv:"party"`
 }
 
 func ReadCsvEvents(urls []string) ([]CsvEvent, error) {
@@ -72,7 +112,18 @@ func ReadCsvEvents(urls []string) ([]CsvEvent, error) {
 }
 
 func (e CsvEvent) String() string {
-	return fmt.Sprintf("Title: %s\nTime: %s\nDescription: %s\n", e.Summary, e.DtStart, e.Summary)
+	return fmt.Sprintf("Title: %s\nTime: %s\nSummary: %s\n", e.Summary, e.DtStart, e.Summary)
+}
+
+func NextCsvEvent(events []CsvEvent) CsvEvent {
+	var now = time.Now()
+	var next CsvEvent
+	for _, e := range events {
+		if e.DtStart.After(now) && (next.DtStart.IsZero() || e.DtStart.Before(next.DtStart.Time)) {
+			next = e
+		}
+	}
+	return next
 }
 
 type IcsEvent struct {
@@ -83,7 +134,7 @@ type IcsEvent struct {
 }
 
 func (e IcsEvent) String() string {
-	return fmt.Sprintf("Title: %s\nTime: %s\nDescription: %s\nLecture: %t", e.Name, e.TimeStamp, e.Description, e.Lecture)
+	return fmt.Sprintf("Title: %s\nTime: %s\nSummary: %s\nLecture: %t", e.Name, e.TimeStamp, e.Description, e.Lecture)
 }
 
 func (e *IcsEvent) Error() string {
--- uit_calendar_util_go/calendar_util_test.go
+++ uit_calendar_util_go/calendar_util_test.go
@@ -1,9 +1,12 @@
 package calendar_util
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCSVReadEvents(t *testing.T) {
-	urls := []string{"https://tp.uio.no/uit/timeplan/excel.php?type=course&sort=week&id[]=INF-3203%2C1&id[]=INF-3701%2C1"}
+	urls := []string{"https://tp.uio.no/uit/timeplan/excel.php?type=course&sort=week&id[]=INF-3203%2C1&id[]=INF-3701%2C1", "https://tp.uio.no/ntnu/timeplan/excel.php?type=courseact&id%5B%5D=GEOG2023%C2%A4&id%5B%5D=KULMI2710%C2%A4&sem=22v&stop=1"}
 	csv, err := ReadCsvEvents(urls)
 	if err != nil {
 		t.Error(err)
@@ -12,6 +15,15 @@ func TestCSVReadEvents(t *testing.T) {
 	for _, event := range csv {
 		t.Log(event)
 	}
+}
+
+func TestNextCsvEvent(t *testing.T) {
+	urls := []string{"https://tp.uio.no/uit/timeplan/excel.php?type=course&sort=week&id[]=INF-3203%2C1&id[]=INF-3701%2C1", "https://tp.uio.no/ntnu/timeplan/excel.php?type=courseact&id%5B%5D=GEOG2023%C2%A4&id%5B%5D=KULMI2710%C2%A4&sem=22v&stop=1"}
+	events, err := ReadCsvEvents(urls)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(NextCsvEvent(events))
 }
 
 func TestIcsGetEvents(t *testing.T) {
@@ -23,4 +35,13 @@ func TestIcsGetEvents(t *testing.T) {
 	for _, event := range res {
 		t.Log(event)
 	}
+}
+
+func TestIcsNextEvent(t *testing.T) {
+	url := "https://tp.uio.no/ntnu/timeplan/ical.php?sem=22v&id%5B0%5D=GEOG2023%C2%A4&id%5B1%5D=KULMI2710%C2%A4&type=courseact"
+	res, err := GetData(url)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(NextIcsEvent(res))
 }
